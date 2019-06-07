e034db67d6c20e3bcd9e84af5ac78595c1c8486b
Jacques Menard <jacquesp.menard@yahoo.com>
2019-06-06 18:45:45 -0700

    Implemented a basic pause functionality and pause menu

---
+++ Assets/Scripts/GameManagers/PauseManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+    }
+    //Pauses the game and activates the pause menu.
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+        
+        if (ui.activeSelf == true)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        //TODO implement main menu.
+        Debug.Log("Go to menu.");
+    }
+}
